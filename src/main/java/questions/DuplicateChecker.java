80859e56f8b35f1924ec0dbe6c729a847899d5a2
msadley <mendes.adley@gmail.com>
2025-07-03 15:57:07 -0300

    Implemented 'DuplicateChecker'

--- src/main/java/questions/DuplicateChecker.java
+++ src/main/java/questions/DuplicateChecker.java
@@ -3,7 +3,29 @@ package questions;
 public class DuplicateChecker {
 
     public boolean containsDuplicates(int[] sequence) {
-        // TODO: Implement the logic for integer arrays.
+        if (sequence == null)
+            return false;
+
+        if (sequence.length == 0)
+            return false;
+
+        int largest = sequence[0];
+        for (int i : sequence)
+            if (i > largest)
+                largest = i;
+
+        int smallest = sequence[0];
+        for (int i : sequence)
+            if (i < smallest)
+                smallest = i;
+
+        boolean[] h = new boolean[largest-smallest+2];
+
+        for (int i : sequence) {
+            if (h[i-smallest])
+                return true;
+            h[i-smallest] = true;
+        }
         return false;
     }
 }
