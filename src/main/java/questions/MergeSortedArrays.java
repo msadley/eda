040c1c8f886a3e41a4b069cae436ebb974fe9b0f
msadley <mendes.adley@gmail.com>
2025-07-03 16:03:41 -0300

    Implemente merge

--- src/main/java/questions/MergeSortedArrays.java
+++ src/main/java/questions/MergeSortedArrays.java
@@ -2,21 +2,24 @@ package questions;
 
 public class MergeSortedArrays {
 
-    /**
-     * Combines two sorted arrays into a single sorted array.
-     * <p>
-     * Restriction: The solution must be implemented without using the language's
-     * sorting function (like Arrays.sort()) after the combination.
-     * The method must have a linear time complexity O(n + m), where n and m are
-     * the sizes of the arrays.
-     *
-     * @param nums1 The first sorted array.
-     * @param nums2 The second sorted array.
-     * @return A new array containing all the elements of nums1 and nums2 in
-     *         ascending order.
-     */
-    public int[] merge(int[] nums1, int[] nums2) {
-        // TODO: Implement your logic here.
-        return new int[0];
+    public int[] merge(int[] a, int[] b) {
+        int[] v = new int[a.length + b.length];
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j])
+                v[k++] = a[i++];
+            else
+                v[k++] = b[j++];
+        }
+
+        while (i < a.length)
+            v[k++] = a[i++];
+        
+        while (j < b.length)
+            v[k++] = b[j++];
+
+        return v;
     }
 }
